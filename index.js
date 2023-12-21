763f79874f7747fddbe1f8a326a53289d743224c
Pranay Alone <64677840+palone12@users.noreply.github.com>
2023-12-21 15:59:04 +0530

    Javascript Fundamentals

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log("Hello World");
+
+// Variable in JS - Container to store value
+
+const firstName = "Pranay";
+const lastName = "Alone";
+
+const accountId = 12121;
+const email = "alonepranay123@gmail.com";
+var password = "****";
+let userName = "Palone23";
+
+console.table([firstName, lastName, accountId, email, password, userName]);
