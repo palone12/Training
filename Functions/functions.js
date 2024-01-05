b85e105df87f10e4fb636cd125f35430d59a0340
Pranay Alone <64677840+palone12@users.noreply.github.com>
2024-01-05 16:55:13 +0530

    Javascript Functions Detailed

---
+++ Functions/functions.js
@@ -0,0 +1,10 @@
+function calculateCartItem(...args) {
+  // using spread operator for unknown number of arguments
+  return args;
+}
+
+function calculateCartItem(val1, val2, ...args) {
+  return args;
+}
+
+console.log(calculateCartItem(1, 23, 3));
